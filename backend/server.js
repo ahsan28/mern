5b83f5b0b2aef75fcde170c9425eeabc3ba5e30a
ahsan28 <ahabib.j@gmail.com>
2021-09-12 09:25:47 +0800

    Init: back and front

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const notes = require("./data/notes")
+const dotenv = require("dotenv")
+
+
+const PORT = process.env.PORT || 5000
+const app = express()
+
+
+app.get('/',(req,res)=>{
+    res.send("My API is running. . .")
+})
+
+app.get('/api/notes',(req, res)=>{
+    res.json(notes)
+})
+
+app.get('/api/notes/:id',(req, res)=>{
+    const note = notes.find(x=>x._id===req.params.id)
+    res.json(note)
+})
+
+app.listen(PORT, console.log("Server started on port 5000"));
---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <h1 className="App">
+      Hello world from Ahsan
+
+    </h1>
+  );
+}
+
+export default App;
